fe417e148a2b1f148a6b30f6fe3c3b3c67b14777
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-25 11:06:56 -0700

    plants table added

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -6,27 +6,27 @@ exports.up = async (knex) => {
       users.string('password', 200).notNullable()
       users.string('user_email', 200).nullable()
       users.string('user_phone', 12).notNullable().unique()//000-000-000
-      users.timestamps(false, true)
-      //users.string('created_at',{ precision: 6 }).defaultTo(knex.fn.now())
+      //users.timestamps(false, true)
+      users.string('created_at',{ precision: 6 }).defaultTo(knex.fn.now())
     })
-    // .createTable('plants', (plants) => {
-    //   plants.increments('plant_id')
-    //   plants.string('plant_nickname', 200).notNullable().unique()
-    //   plants.string('plant_species', 200).notNullable()
-    //   plants.string('h2ofrequency', 200).notNullable()
-    //   plants.string('plant_image', 200).nullable()
-    //   // plants.integer('user_id')
-    //   // .unsigned()
-    //   // .notNullable()
-    //   // .references('user_id')
-    //   // .inTable('users')
-    //   // .onUpdate('RESTRICT')
-    //   // .onDelete('RESTRICT')
-    //   plants.timestamps(false, true);
-    // })
+    .createTable('plants', (plants) => {
+      plants.increments('plant_id')
+      plants.string('plant_nickname', 200).notNullable().unique()
+      plants.string('plant_species', 200).notNullable()
+      plants.string('h2ofrequency', 200).notNullable()
+      plants.string('plant_image', 200).nullable()
+      plants.integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('user_id')
+      .inTable('users')
+      .onUpdate('RESTRICT')
+      .onDelete('RESTRICT')
+      plants.timestamps(false, true);
+    })
 }
 
 exports.down = async (knex) => {
-  //await knex.schema.dropTableIfExists('plants')
+  await knex.schema.dropTableIfExists('plants')
   await knex.schema.dropTableIfExists('users')
 }
