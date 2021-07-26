02417e5943cc8e4f7fb09bf969753cab8311e376
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-26 07:47:02 -0700

    added secret and token-builder file

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,50 @@
 const router = require("express").Router();
-//const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const Users = require("../users/users-model");
-//const tokenBuilder = require('./token-builder')
+const tokenBuilder = require('./token-builder')
 //const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-//const { JWT_SECRET } = require("../secrets"); // use this secret!
+
+
+
+router.post("/register",  (req, res, next) => {
+    
+    let user ={
+      username :req.body.username,
+      password: req.body.password
+    }
+  
+    const rounds = process.env.BCRYPT_ROUNDS || 8; // 2 ^ 8
+    const hash = bcrypt.hashSync(user.password, rounds);
+  
+    user.password = hash
+   
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(next);
+  });
+  
+  router.post('/login', (req, res, next) => {
+      
+   let { username, password } = req.body;
+    Users.findBy({ username }) 
+      .then(([user]) => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          // generate a token and send it back
+          const token = tokenBuilder(user)
+          // the client will provide token in future requests
+          res.status(200).json({
+            message: `welcome, ${user.username}`,
+            token,
+          });
+        } else {
+          next({ status: 401, message: 'invalid credentials' });
+        }
+      })
+      .catch(next);
+  });
+
+
 
 module.exports = router;
--- api/data/seeds/02-users.js
+++ api/data/seeds/02-users.js
@@ -5,11 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {username: 'lambda'},
-        {password: 'lambda123'},
-        {user_email: 'contact@lambdaschool.com'},
-        {user_phone: '415-262 4219'}
-        
+        {username: 'lambda',password: 'lambda123', user_email: 'contact@lambdaschool.com', user_phone: '415-262 4219'}
+               
       ]);
     });
 };
